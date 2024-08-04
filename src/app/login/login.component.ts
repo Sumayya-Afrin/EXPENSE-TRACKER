3053b32d31e0e84b6c2f2cc96206ed2a67413bc8
Sumayya-Afrin <sumayyafrin.mohammad@proclink.com>
2024-08-04 11:49:06 +0530

    First commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+  loginForm!: FormGroup;
+
+  navigateToRegister() {
+    throw new Error('Method not implemented.');
+  }
+  onLogin() {
+    throw new Error('Method not implemented.');
+  }
+}
